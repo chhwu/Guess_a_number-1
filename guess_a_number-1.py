4a63aea5066a782a1155118a0ede27b3ea65cb2e
chhwu <76684717+chhwu@users.noreply.github.com>
2021-05-24 14:36:35 +0800

    Add files via upload

---
+++ guess_a_number-1.py
@@ -0,0 +1,21 @@
+import random
+print('Let\'s gess a number!.')
+
+num = random.randint(1, 100)
+count = 0
+
+while True:
+    guess = int(input('Guess a number from 1 to 100: '))
+    if guess > num:
+        count += 1
+        print('Guess smaller.')
+    elif guess < num:
+        count += 1
+        print('Guess bigger.')
+    else:
+        count += 1
+        print('Congrats! The number is %d.' % num)
+        if count == 1:
+            print('You took 1 try to reach to the correct number.')
+        print('You took %d tries to reach to the number.' % count)
+        break
